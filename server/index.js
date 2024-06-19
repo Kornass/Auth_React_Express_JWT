be2e230dd479d8357a882f2a8e861262a5f35ec4
Paweł Kornas <kornasnorge@gmail.com>
2024-06-19 14:10:45 +0200

    Move

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const port = process.env.PORT || 4040;
 require("dotenv").config();
 const connectDB = require("./config/db");
-const {errorHandler} = require('./middlewares/errorMiddleware')
+const { errorHandler } = require("./middlewares/errorMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-connectDB();
 
 app.use("/users", require("./router/usersRouter"));
 
 // errors going to error handler after calling next(error) in controller or throwing error with throw new Error()
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+connectDB().then(() => {
+  app.listen(port, () => console.log(`listening on port ${port}`));
+});
